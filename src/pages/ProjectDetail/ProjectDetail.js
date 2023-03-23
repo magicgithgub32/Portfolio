5fa4c381b8fefe00421183d0fcd80d05d4ae4381
Rubcs <Rubcs@gmail.com>
2023-03-23 07:18:25 +0100

    update content in Projects file trying to make the addEventListener work

--- src/pages/ProjectDetail/ProjectDetail.js
+++ src/pages/ProjectDetail/ProjectDetail.js
@@ -4,17 +4,19 @@ import { projectsData } from "../../data/projectsData";
 export const ProjectDetail = (id) => {
   const main = document.querySelector("main");
   main.innerHTML = `
-  <div class="projectDetail-container"></div>
-    
-    `;
+    <div class="projectDetail-container"></div>
+  `;
 
-  const project = projectsData.find((ProjectDetail) => ProjectDetail.id === id);
+  const project = projectsData.find((projectDetail) => projectDetail.id === id);
 
   const container = document.querySelector(".projectDetail-container");
+
   container.innerHTML += `
-      <h2>${project.detail.titlePost}</h2>
-      
-      <div class="detail-img-container"><img src=${project.detail.imageDetail} alt=${project.detail.titleDetail} /></div>
-      <p>${project.detail.descDetail}</p>
-    `;
+    <h2>${project.projectDetail.titleDetail}</h2>
+    ${Divider()}
+    <div class="detail-img-container"><img src=${
+      project.projectDetail.imageDetail
+    } alt=${project.projectDetail.titleDetail} /></div>
+    <p>${project.projectDetail.descDetail}</p>
+  `;
 };
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -10,7 +10,7 @@ export const Projects = () => {
   // cleanPage(main);
   main.innerHTML = `
     <section class="projects">
-    <h2>These are <span>Projects</span> I have been doing lately</h2>
+    <h2>These are <span>Projects</span> I've been doing lately</h2>
     <div class="projects-gallery">
         ${projectsData.map((project) => ProjectCard(project)).join("")}
     </div>
@@ -18,15 +18,13 @@ export const Projects = () => {
     `;
 
   const addEventListenerForProjectDetail = (id) => {
-    const project = document.querySelector(`#project${id}`);
+    const project = document.getElementById(id);
     project.addEventListener("click", () => {
       ProjectDetail(id);
     });
   };
 
   for (const project of projectsData) {
-    const projectCard = ProjectCard(project);
-    main.querySelector(".projects-gallery").innerHTML += projectCard;
     addEventListenerForProjectDetail(project.id);
   }
 };
