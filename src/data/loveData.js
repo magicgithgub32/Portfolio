a2bf103ee7796b783b66c15320ae973ce3dfa92c
Rubcs <Rubcs@gmail.com>
2023-04-04 16:26:40 +0200

    add SF,DB & B2F pics

--- src/data/loveData.js
+++ src/data/loveData.js
@@ -54,4 +54,19 @@ export const loveData = [
     picture:
       "https://res.cloudinary.com/dxxkog06n/image/upload/v1680153696/Screenshot_2023-03-30_at_07.17.21_y81wrm.png",
   },
+  {
+    title: "DB",
+    picture:
+      "https://res.cloudinary.com/dxxkog06n/image/upload/v1680618117/522278_mtqash.jpg",
+  },
+  {
+    title: "Back2TheFuture",
+    picture:
+      "https://res.cloudinary.com/dxxkog06n/image/upload/v1680617608/96374601_3021492434585302_8994096825644351488_n_xxemnv.jpg",
+  },
+  {
+    title: "SF2",
+    picture:
+      "https://res.cloudinary.com/dxxkog06n/image/upload/v1680618132/street-fighter-ken_sqridi.gif",
+  },
 ];
